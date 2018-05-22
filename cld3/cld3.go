75d5a1fbf00aba0337eb06cf66d46781945668d2
Jeff Hodges <jeff@somethingsimilar.com>
2018-05-22 01:02:03 -0700

    initial commit

---
+++ cld3/cld3.go
@@ -0,0 +1,40 @@
+//go:generate protoc --cpp_out=./cld_3/protos feature_extractor.proto sentence.proto task_spec.proto
+
+// Package cld3 implements language detection using the Compact Language Detector v3.
+//
+// This packages includes the relevant sources from the CLD3 project, so it doesn't require any external dependencies. For more information on CLD3, see https://github.com/google/cld3/ .
+package cld3
+
+// #cgo CPPFLAGS: -std=c++11
+// #include <stdlib.h>
+// #include "cld3.h"
+import "C"
+import "unsafe"
+
+// FindLanguageOfValidUTF8 detects the language in a given text. The Result may be FIXME
+func FindLanguageOfValidUTF8(text string) Result {
+	cs := C.CString(text)
+	defer C.free(unsafe.Pointer(cs))
+	res := C.FindLanguageOfValidUTF8(cs, -1)
+	r := Result{}
+	r.Language = C.GoStringN(res.language, res.len_language)
+	r.Probability = float32(res.probability)
+	r.IsReliable = bool(res.is_reliable)
+	r.Proportion = float32(res.proportion)
+	return r
+}
+
+type Result struct {
+	Language string
+
+	// Probability is the probability from 0 to 1 of the text being in the
+	// returned Language.
+	Probability float32
+
+	// IsReliable is true when the prediction is reliable.
+	IsReliable bool
+
+	// Proportion of bytes associated with the language. If FindLanguage is
+	// called, this variable is set to 1.
+	Proportion float32
+}
---
+++ cld3/cld3.h
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  typedef struct {
+    char* language;
+    size_t len_language;
+    float probability; // Language probability.
+    bool is_reliable;  // Whether the prediction is reliable.
+
+    // Proportion of bytes associated with the language. If FindLanguage is
+    // called, this variable is set to 1.
+    float proportion;
+  } Result;
+
+  const Result FindLanguageOfValidUTF8(char *data, int length);
+
+#ifdef __cplusplus
+}
+#endif
+
